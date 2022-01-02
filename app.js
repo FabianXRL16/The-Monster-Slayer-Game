6cc8d8210fad5796d1e845657a8ce53605b9db46
FabianXRL16 <fabianpacherres@gmail.com>
2022-01-02 17:40:12 -0500

    Start new game

--- app.js
+++ app.js
@@ -44,6 +44,13 @@ const app = Vue.createApp({
     },
   },
   methods: {
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+      this.winner = null;
+      this.gameOver = false;
+    },
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandom(5, 12);
